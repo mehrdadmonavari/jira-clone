5ef5f407281db869021b230b750f844509a5b35a
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-10-28 23:04:50 +0330

    create use-logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,32 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+   const router = useRouter();
+   const { data, isLoading } = useCurrent();
+   const { mutate } = useLogout();
+
+   useEffect(() => {
+      if (!data && !isLoading) router.push("/sign-in");
+   }, [data]);
+
+   if (isLoading)
+      <div className="flex justify-center items-center text-2xl text-center py-10">
+         Loading ...
+      </div>;
+
    return (
-      <div className="flex justify-center items-center">
-         <Button>default</Button>
-         <Button variant="secondary">secondary</Button>
-         <Button variant="destructive">destructive</Button>
-         <Button variant="ghost">ghost</Button>
-         <Button variant="muted">link</Button>
-         <Button variant="outline">outline</Button>
-         <Button variant="teritary">outline</Button>
+      <div className="flex flex-col justify-center items-center text-2xl text-center py-8">
+         this is vislble obly to outhorized users. if your not, you will redirect to
+         signUp/signIn page.
+         <Button onClick={() => mutate()} className="px-6 mt-4">
+            logout
+         </Button>
       </div>
    );
 }
