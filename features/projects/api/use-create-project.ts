1e280ac08e300b471aef3279322367b8167a16b4
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-11-18 22:37:29 +0330

    working on create project functionality

---
+++ features/projects/api/use-create-project.ts
@@ -0,0 +1,30 @@
+import { client } from "@/lib/rpc";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { toast } from "sonner";
+
+type RequerstType = InferRequestType<(typeof client.api.projects)["$post"]>;
+type ResponseType = InferResponseType<(typeof client.api.projects)["$post"]>;
+
+export const useCreateProject = () => {
+   const queryClient = useQueryClient();
+
+   const mutation = useMutation<ResponseType, Error, RequerstType>({
+      mutationFn: async ({ form }) => {
+         const response = await client.api.projects["$post"]({ form });
+
+         if (!response.ok) throw new Error("Faild to create project");
+
+         return await response.json();
+      },
+      onSuccess: () => {
+         toast.success("Project created");
+         queryClient.invalidateQueries({ queryKey: ["projects"] });
+      },
+      onError: () => {
+         toast.error("Faild to create projects");
+      },
+   });
+
+   return mutation;
+};
